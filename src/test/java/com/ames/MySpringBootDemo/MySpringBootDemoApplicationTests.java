25288e1244180bb571b18c7ee0b27249f43f9893
Kou <ykou@itc-imac-29.ur.rochester.edu>
2023-11-03 20:43:23 -0400

    init project

---
+++ src/test/java/com/ames/MySpringBootDemo/MySpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ames.MySpringBootDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
